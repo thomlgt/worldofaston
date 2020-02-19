123659a95e91cf95b9b95ed53f97c1037f2ed5a8
Thomas.L <thomlgt@hotmail.fr>
2020-02-19 10:22:22 +0100

    Exercice 2 finit

---
+++ Monde.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WorldOfAston
+{
+    static class Monde
+    {
+        /// <summary>
+        /// Cette méthode statique permet de créer un nouveau personnage en demandant son nom à l'utilisateur
+        /// </summary>
+        /// <returns></returns>
+        public static Personnage PersonnageFactory()
+        {
+            Console.WriteLine("Saisissez le nom du personnage :");
+            string nom = Console.ReadLine();
+            Personnage personnage = new Personnage(nom, 100, 10);
+            return personnage;
+        }
+    }
+}
--- Personnage.cs
+++ Personnage.cs
@@ -23,18 +23,6 @@ namespace WorldOfAston
         }
 
         /// <summary>
-        /// Cette méthode statique permet de créer un nouveau personnage en demandant son nom à l'utilisateur
-        /// </summary>
-        /// <returns></returns>
-        public static Personnage PersonnageFactory()
-        {
-            Console.WriteLine("Saisissez le nom du personnage :");
-            string nom = Console.ReadLine();
-            Personnage personnage = new Personnage(nom, 100, 10);
-            return personnage;
-        }
-
-        /// <summary>
         /// Cette méthode surcharge la méthode ToString pour afficher un personnage
         /// </summary>
         /// <returns></returns>
